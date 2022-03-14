696b809484499c33b1f450b5e582c3b1ff2b2297
Dragon <oliverkrue@gmail.com>
2022-03-14 23:28:19 +0100

    resetet to information logging

--- NeutraleNull.GuidToSteamIdApi/Program.cs
+++ NeutraleNull.GuidToSteamIdApi/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(
 builder.Services.AddTransient<ISeedDatabaseUseCase, SeedDatabaseUseCase>();
 builder.Services.AddHostedService<DataGenerationService>();
 
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
